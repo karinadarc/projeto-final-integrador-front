9160bdea6109c3ed409f030cf81c364a7922cbb2
karinadarc <darcsilva.kd@gmail.com>
2024-04-21 19:27:15 -0300

    Cria componente MyButton

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import useRequestData from "../Hooks/useRequestData";
 import MainContainer from "../componentes/MainContainer";
 import CardPost from "../componentes/Card";
-import { Center, Spinner, SimpleGrid, Divider } from "@chakra-ui/react";
+import { Center, Spinner, SimpleGrid, Divider, Spacer } from "@chakra-ui/react";
 import HeaderPrincipal from "../componentes/HeaderPrincipal";
 import GlobalContext from "../context/GlobalContext";
+import MyButton from "../componentes/MyButton";
 
 function HomePage() {
   const context = useContext(GlobalContext);
@@ -25,8 +26,9 @@ function HomePage() {
   return (
     <MainContainer>
       <HeaderPrincipal exibirX={false}></HeaderPrincipal>
-      <Divider />
-      <SimpleGrid columns={1} spacingY={"10px"}>
+      <Spacer />
+      <SimpleGrid marginTop={"10px"} columns={1} spacingY={"10px"}>
+        <MyButton>Adicionar novo post</MyButton>
         {isLoading ? (
           <Center>
             <Spinner />
